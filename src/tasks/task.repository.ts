6944a61b31f31bc5ec217b9a13c31e5228790f72
Sergey_M <snmartusenko@gmail.com>
2020-01-05 14:04:29 +0200

    added logger examples

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -4,12 +4,13 @@ import { TaskStatus } from './task-status.enum';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { User } from '../auth/user.entity';
-
+import { InternalServerErrorException, Logger } from '@nestjs/common';
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
-  async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
+  private logger = new Logger('TaskRepository');
 
+  async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
     const { status, search } = filterDto;
     const query = this.createQueryBuilder('task');
 
@@ -26,8 +27,13 @@ export class TaskRepository extends Repository<Task> {
     }
     // console.log(query);
 
-    const models = await query.getMany();
-    return models;
+    try {
+      const models = await query.getMany();
+      return models;
+    } catch (e) {
+      this.logger.error(`Failed to get tasks for user "${user.username}". Filter: ${JSON.stringify(filterDto)}`, e.stack);
+      throw new InternalServerErrorException();
+    }
   }
 
   async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
@@ -37,7 +43,13 @@ export class TaskRepository extends Repository<Task> {
     model.title = title;
     model.description = description;
     model.status = TaskStatus.OPEN;
-    await model.save();
+
+    try {
+      await model.save();
+    } catch (e) {
+      this.logger.error(`Failed to create task for user "${user.username}". Data: ${JSON.stringify(createTaskDto)}`, e.stack);
+      throw new InternalServerErrorException();
+    }
 
     // delete user field from response
     delete model.user;
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -2,7 +2,7 @@ import {
   Body,
   Controller,
   Delete,
-  Get,
+  Get, Logger,
   Param, ParseIntPipe,
   Patch,
   Post,
@@ -23,6 +23,8 @@ import { GetUser } from '../auth/get-user.decorator';
 @Controller('tasks')
 @UseGuards(AuthGuard())
 export class TasksController {
+  private logger = new Logger('TasksController');
+
   constructor(private tasksService: TasksService) {
   }
 
@@ -31,6 +33,7 @@ export class TasksController {
     @Query(ValidationPipe) filterDto: GetTasksFilterDto,
     @GetUser() user: User,
   ): Promise<Task[]> {
+    this.logger.verbose(`User "${user.username}" retrieving all tasks. Filter: ${JSON.stringify(filterDto)}`);
     return this.tasksService.getTasks(filterDto, user);
   }
 
@@ -40,6 +43,7 @@ export class TasksController {
     @Body() createTaskDto: CreateTaskDto,
     @GetUser() user: User,
   ): Promise<Task> {
+    this.logger.verbose(`User "${user.username}" creating a new task. Data: ${JSON.stringify(createTaskDto)}`);
     const model = this.tasksService.createTask(createTaskDto, user);
     return model;
   }
